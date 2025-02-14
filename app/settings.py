b65a25395cf51f67af5a549bdc2b93acdcdf0054
kdein <kde.denardi@gmail.com>
2025-02-14 14:58:57 -0400

    Carregando imagens

--- app/settings.py
+++ app/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'  # URL base para arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Caminho local (desenvolvimento)
+]
+STATIC_ROOT = r'X:\BI\VANDER\sistema\sistem\static'  # Caminho absoluto para produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
